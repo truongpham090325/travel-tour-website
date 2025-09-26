72f608e820c12089af89609fb879ba816d70b4a3
Phạm Quang Trường <truongpham090325@gmail.com>
2025-09-26 15:54:21 +0700

    1.14 -> 1.19

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,6 +4,7 @@ const dashboardRoutes = require("./dashboard.route");
 const categoryRoutes = require("./category.route");
 const tourRoutes = require("./tour.route");
 const orderRoutes = require("./order.route");
+const settingRoutes = require("./setting.route");
 
 router.use("/account", accountRoutes);
 
@@ -14,5 +15,7 @@ router.use("/category", categoryRoutes);
 router.use("/tour", tourRoutes);
 
 router.use("/order", orderRoutes);
+
+router.use("/setting", settingRoutes);
 
 module.exports = router;
