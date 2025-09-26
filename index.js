7cddcca9cf66e8a81b660626bfb32b60d6b0f0c4
Phạm Quang Trường <truongpham090325@gmail.com>
2025-09-26 10:47:46 +0700

    4.4. Sửa đường dẫn admin linh động

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const databaseConfig = require("./config/database.config");
 const clientRoutes = require("./routes/client/index.route");
 const adminRoutes = require("./routes/admin/index.route");
+const variableConfig = require("./config/variable.config");
 require("dotenv").config();
 const app = express();
 const port = 3000;
@@ -19,9 +20,12 @@ app.set("view engine", "pug");
 //Thiết lập thư mục chứa file tĩnh
 app.use(express.static(path.join(__dirname, "public")));
 
+//Tạo biến toàn cục trong file Pug
+app.locals.pathAdmin = variableConfig.pathAdmin;
+
 app.use("/", clientRoutes);
 
-app.use("/admin", adminRoutes);
+app.use(`/${variableConfig.pathAdmin}`, adminRoutes);
 
 app.listen(port, () => {
   console.log(`Website đang chạy trên cổng ${port}`);
