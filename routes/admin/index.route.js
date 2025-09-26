3bef755287f4d43f250140c0616142e83c42f15a
Phạm Quang Trường <truongpham090325@gmail.com>
2025-09-26 14:54:24 +0700

    1.5 + 1.6

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const accountRoutes = require("./account.route");
 const dashboardRoutes = require("./dashboard.route");
+const categoryRoutes = require("./category.route");
 
 router.use("/account", accountRoutes);
 
 router.use("/dashboard", dashboardRoutes);
+
+router.use("/category", categoryRoutes);
 
 module.exports = router;
