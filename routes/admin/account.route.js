29ab89a107b3b02d249e9b11f8d9c0f94a542637
Phạm Quang Trường <truongpham090325@gmail.com>
2025-09-26 10:32:48 +0700

    4.3. Tích hợp giao diện trang Đăng ký

--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -3,4 +3,6 @@ const accountController = require("../../controllers/admin/account.controller");
 
 router.get("/login", accountController.login);
 
+router.get("/register", accountController.register);
+
 module.exports = router;
