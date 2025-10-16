4bb463e2adb2b84449083b3e0d863da8acbf4162
Phạm Quang Trường <truongpham090325@gmail.com>
2025-10-16 17:24:44 +0700

    04. Hiển thị data ra Section 2 (Trang chủ)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,31 @@
-module.exports.home = (req, res) => {
+const Tour = require("../../models/tour.model");
+const moment = require("moment");
+
+module.exports.home = async (req, res) => {
+  // Section 2
+  const tourListSection2 = await Tour.find({
+    status: "active",
+    deleted: false,
+  })
+    .sort({
+      position: "desc",
+    })
+    .limit(6);
+
+  for (const item of tourListSection2) {
+    item.discount = Math.floor(
+      ((item.priceAdult - item.priceNewAdult) / item.priceAdult) * 100
+    );
+    if (item.departureDate) {
+      item.departureDateFormat = moment(item.departureDate).format(
+        "DD/MM/YYYY"
+      );
+    }
+  }
+  // End Section 2
+
   res.render("client/pages/home", {
     pageTitle: "Trang chủ",
+    tourListSection2: tourListSection2,
   });
 };
