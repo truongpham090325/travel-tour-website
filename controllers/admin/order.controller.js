21450289df0f4c4b931639df790e1ab14f582e54
Phạm Quang Trường <truongpham090325@gmail.com>
2025-10-24 21:24:53 +0700

    Tính năng bộ lọc + Tìm kiếm + Phân trang cho trang Quản lý đơn hàng

--- controllers/admin/order.controller.js
+++ controllers/admin/order.controller.js
@@ -12,6 +12,49 @@ module.exports.list = async (req, res) => {
     deleted: false,
   };
 
+  // Lọc theo trạng thái
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+  // Hết lọc theo trạng thái
+
+  // Lọc theo ngày tạo
+  const dataFilter = {};
+  if (req.query.startDate) {
+    const startDate = moment(req.query.startDate).toDate();
+    dataFilter.$gte = startDate;
+  }
+  if (req.query.endDate) {
+    const endDate = moment(req.query.endDate).toDate();
+    dataFilter.$lte = endDate;
+  }
+  if (Object.keys(dataFilter).length > 0) {
+    find.createdAt = dataFilter;
+  }
+  // Hết lọc theo ngày tạo
+
+  // Tìm kiếm
+  if (req.query.keyword) {
+    find.fullName = req.query.keyword;
+  }
+  // Hết tìm kiếm
+
+  // Phân trang
+  const limitItems = 3;
+  let page = 1;
+  if (req.query.page && parseInt(req.query.page) > 0) {
+    page = parseInt(req.query.page);
+  }
+  const skip = (page - 1) * limitItems;
+  const totalRecord = await Order.countDocuments(find);
+  const totalPage = Math.ceil(totalRecord / limitItems);
+  const pagination = {
+    skip: skip,
+    totalRecord: totalRecord,
+    totalPage: totalPage,
+  };
+  // Hết phân trang
+
   const orderList = await Order.find(find).sort({
     createdAt: "desc",
   });
@@ -48,6 +91,7 @@ module.exports.list = async (req, res) => {
   res.render("admin/pages/order-list", {
     pageTitle: "Quản lý đơn hàng",
     orderList: orderList,
+    pagination: pagination,
   });
 };
 
