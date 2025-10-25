1ca5b56f30b2e19901c79e703fcb0f9a8e3beb2e
Phạm Quang Trường <truongpham090325@gmail.com>
2025-10-25 15:03:03 +0700

    3.1. Khối Thông số tổng quan

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,31 @@
+const AccountAdmin = require("../../models/account-admin.model");
+const Order = require("../../models/order.model");
+
 module.exports.dashboard = async (req, res) => {
+  // Thông số tổng quan
+  const overview = {
+    totalAdmin: 0,
+    totalOrder: 0,
+    totalRevenue: 0,
+  };
+
+  overview.totalAdmin = await AccountAdmin.countDocuments({
+    deleted: false,
+  });
+
+  const orderList = await Order.find({
+    deleted: false,
+  });
+
+  overview.totalOrder = orderList.length;
+  overview.totalRevenue = orderList.reduce(
+    (total, item) => total + item.total,
+    0
+  );
+  // Hết thông số tổng quan
+
   res.render("admin/pages/dashboard", {
     pageTitle: "Tổng quan",
+    overview: overview,
   });
 };
